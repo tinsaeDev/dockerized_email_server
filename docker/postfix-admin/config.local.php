e735b4a4ead817fdbb9e6cce70b3a010df5ff82a
SAZX <tinsaedev@gmail.com>
2022-10-27 14:28:38 +0300

    IMAP working

--- docker/postfix-admin/config.local.php
+++ docker/postfix-admin/config.local.php
@@ -6,8 +6,9 @@ $CONF['database_port'] = '5432';
 $CONF['database_user'] = 'mail1234567890';
 $CONF['database_password'] = 'mail1234567890';
 $CONF['database_name'] = 'mail1234567890';
-$CONF['encrypt'] = 'dovecot:ARGON2I';
 $CONF['dovecotpw'] = "/usr/bin/doveadm pw -r 5";
+$CONF['encrypt'] = 'dovecot:ARGON2I';
+$CONF['encrypt'] = 'md5';
 $CONF['setup_password'] = '$2y$10$th72KE8xFcl4xccXI8gJzeBeUpIY9h3O06MgVsKkDB3Lf5GsB.Kxi';
 
 
