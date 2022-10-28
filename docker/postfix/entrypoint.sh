12515aaeda7128614c4d49d6774974303c004f43
SAZX <tinsaedev@gmail.com>
2022-10-28 17:44:56 +0300

    Added comments

--- docker/postfix/entrypoint.sh
+++ docker/postfix/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 mkdir -p /var/spool/postfix/etc/
 
+# Postfix runs in chrooted enviroment, so it is needed to copy some important files
 cp /etc/services /var/spool/postfix/etc/services
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf 
+
+
 postfix start-fg
