38ec85089c26e83a9a5984476abcfc35c8e6d65c
SAZX <tinsaedev@gmail.com>
2022-11-01 12:15:13 +0300

    use common postgress server, fixed isseu of dovecot file not been copied

---
+++ docker/dovecot/entrypoint.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+
+echo "Insideeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+
+ls /etc/dovecot/conf.d/
+
+# Start Dovecot
+/usr/sbin/dovecot -F
+# /etc/init.d/dovecot start
+
--- docker/postgress/docker-entrypoint-initdb.d/initial.sql
+++ docker/postgress/docker-entrypoint-initdb.d/initial.sql
@@ -1,4 +1,1 @@
-
-create user roundcubeadmin with encrypted password 'roundcubeadmin_1234';
-create database roundcube;
-grant all privileges on database roundcube to roundcubeadmin;
+CREATE DATABASE ROUNDCUBE_DB;
