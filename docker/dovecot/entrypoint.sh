515c17062caf2077aa9dcf749ba553420f0cb43a
SAZX <tinsaedev@gmail.com>
2022-11-01 15:14:54 +0300

    Made posfix read database connection variables from env file

--- docker/dovecot/entrypoint.sh
+++ docker/dovecot/entrypoint.sh
@@ -2,34 +2,10 @@
 
 # Update the database variables
 
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-
-
-echo "The env sayas : $DB_PASSWORD"
-
 sed -i 's/${DB_HOST}'/"$DB_HOST"/g /etc/dovecot/dovecot-sql.conf.ext 
 sed -i 's/${DB_NAME}'/"$DB_NAME"/g /etc/dovecot/dovecot-sql.conf.ext 
 sed -i 's/${DB_USER}'/"$DB_USER"/g /etc/dovecot/dovecot-sql.conf.ext 
 sed -i 's/${DB_PASSWORD}'/"$DB_PASSWORD"/g /etc/dovecot/dovecot-sql.conf.ext 
-
-less /etc/dovecot/dovecot-sql.conf.ext 
-
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
-echo "#################################"
 
 # Start Dovecot
 /usr/sbin/dovecot -F
--- docker/postfix/entrypoint.sh
+++ docker/postfix/entrypoint.sh
@@ -5,5 +5,40 @@ mkdir -p /var/spool/postfix/etc/
 cp /etc/services /var/spool/postfix/etc/services
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf 
 
+# user = ${DB_USER}
+# password = ${DB_PASSWORD}
+# hosts = ${DB_HOST}
+# dbname = ${DB_NAME}
+
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+for file in /etc/postfix/pgsql/*.cf; do
+    # sed -i 's/asd/dfg/g' $i
+
+    sed -i 's/${DB_HOST}'/"$DB_HOST"/g $file
+    sed -i 's/${DB_NAME}'/"$DB_NAME"/g $file
+    sed -i 's/${DB_USER}'/"$DB_USER"/g $file
+    sed -i 's/${DB_PASSWORD}'/"$DB_PASSWORD"/g $file
+
+   less $file
+done
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+
+
+# Configure database per the enviroment variables
+# sed -i 's/${DB_USER}'/"$DB_USER"/g /etc/dovecot/dovecot-sql.conf.ext 
+# sed -i 's/${DB_USER}'/"$DB_USER"/g /etc/dovecot/dovecot-sql.conf.ext 
+# sed -i 's/${DB_USER}'/"$DB_USER"/g /etc/dovecot/dovecot-sql.conf.ext 
+# sed -i 's/${DB_USER}'/"$DB_USER"/g /etc/dovecot/dovecot-sql.conf.ext 
+
 
 postfix start-fg
