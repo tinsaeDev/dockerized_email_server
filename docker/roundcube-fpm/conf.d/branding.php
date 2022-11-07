e454041f14dc221755f76cd4f4f441a2b1f8ebba
SAZX <tinsaedev@gmail.com>
2022-11-07 13:55:13 +0300

    OOOO

--- docker/roundcube-fpm/conf.d/branding.php
+++ docker/roundcube-fpm/conf.d/branding.php
@@ -35,7 +35,7 @@ $rcmail_config['password_confirm_current'] = true;
 $rcmail_config['password_db_dsn'] = "pgsql://$vmail_db_user:$vmail_db_password@$vmail_db_host/$vmail_db_name";
 
 
-$newpwd = '{ARGON2I}$argon2i$v=19$m=32768,t=4,p=1$4EShHuxNL4Y4MjF2yEcgYQ$xYQBaya1V7zi0CvMWD62UJyWYphxj9LWpJj5hDNSdSw';
+$newpwd = '{ARGON2I}$argon2i$v=19$m=32768,t=4,p=1$J+LeI9bJ6RXWE77WzaiU3g$XDakfb8R1XqMKCjoADB4oliGin7p65TqSniEVkIuwfc';
 // $rcmail_config['password_query'] = "UPDATE mailbox SET password=CONCAT(_utf8'{SHA512-CRYPT}',ENCRYPT(_utf8%p,CONCAT(_utf8'$6$', SUBSTRING(SHA(RAND()), -16)))) WHERE user=%u LIMIT 1";
 $rcmail_config['password_query'] = "UPDATE mailbox SET password='$newpwd',modified=NOW() WHERE username=%u";
 $rcmail_config['password_crypt_hash'] = 'PASSWORD_ARGON2I';
